665af8fabcf395b17cb13d4294f7993cae920629
devthomas <thomas.dev666@gmail.com>
2025-05-10 22:09:12 +0200

    Website - Copy Static - first working draft

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
+import os.path
+import shutil
+
 from textnode import TextNode, TextType
 
 
+def copy_static_to_public()-> None:
+    if os.path.exists("static") == False:
+        print("No static directory")
+        return Exception("No 'static' directory")
+
+
+
+    if os.path.exists("public") == True:
+        shutil.rmtree('public')
+
+    if os.path.exists("public") == False:
+        os.mkdir("public")
+
+
+    shutil.copy("static/index.css", "public")
+    os.mkdir("public/images")
+    shutil.copy("static/images/tolkien.png", "public/images/tolkien.png")
+    return None
+
+
 def main()-> None:
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    # node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    # print(node)
+
+    copy_static_to_public()
 
 if __name__ == "__main__":
     main()
+
+
