6af8985193ee66b602c2c190e64246dfbdcd893a
devthomas <thomas.dev666@gmail.com>
2025-05-13 15:41:55 +0200

    Website - Deploy to the Internet - Build v2

--- src/generate_page.py
+++ src/generate_page.py
@@ -21,8 +21,22 @@ def generate_page(from_path, template_path, dest_path, basepath):
     template_file_with_new_title = template_file.replace("{{ Title }}", title)
     template_file_with_new_content = template_file_with_new_title.replace("{{ Content }}", html_string)
 
+    # Replace any href that starts with a slash
     template_file_with_new_content = re.sub(r'href="/', f'href="{basepath}', template_file_with_new_content)
+
+    # Replace any src that starts with a slash
     template_file_with_new_content = re.sub(r'src="/', f'src="{basepath}', template_file_with_new_content)
+
+    # Also handle relative paths in nested pages (without starting slash)
+    # This is for pages in subdirectories like /blog/
+    if '/' in dest_path:
+        depth = dest_path.count('/') - 1  # How many directories deep we are
+        if depth > 0:
+            # For relative paths (those not starting with /)
+            template_file_with_new_content = re.sub(r'href="(?!http|/|#|mailto:)', f'href="{basepath}',
+                                                    template_file_with_new_content)
+            template_file_with_new_content = re.sub(r'src="(?!http|/|#)', f'src="{basepath}',
+                                                    template_file_with_new_content)
 
     dir_name = os.path.dirname(dest_path)
     if dir_name:
